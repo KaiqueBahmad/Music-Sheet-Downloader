85398d90a93203884a124b726eb9aea1300b12a6
KaiqueBahmad <84449201+KaiqueBahmad@users.noreply.github.com>
2021-12-08 04:02:15 -0300

    Update Finder.py

--- Finder.py
+++ Finder.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -8,7 +7,6 @@ from bs4 import BeautifulSoup
 import requests
 import shutil
 import os
-
 
 
 class Finder():
